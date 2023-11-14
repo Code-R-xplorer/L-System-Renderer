e75d91df05da126321a63d83c91801fad5aa670e
Harry Vallance <harry@robvallance.com>
2023-11-14 15:49:48 +0000

    Testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace L_System_Renderer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
