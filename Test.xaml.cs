c7a3704a0085db04cfa366bf21283f8e13a8353d
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-11-16 10:51:55 +0000

    Koch Curve Implemented

--- Test.xaml.cs
+++ Test.xaml.cs
@@ -23,38 +23,38 @@ namespace L_System_Renderer
     {
         
         private string _axiom = "F";
-        private Dictionary<string, string> _rules;
-        private float _angle = MathF.PI / 3;
+        private Dictionary<char, string> _rules = new()
+        {
+            ['F'] = "F+F--F+F",
+            ['+'] = "+",
+            ['-'] = "-"
+        };
+        private float _angle = 60;
         private float _length = 1;
 
-        private int _iterations = 1;
+        private int _iterations = 5;
 
         private string _final;
 
         public Test()
         {
-            _rules = new Dictionary<string, string>();
-            _rules["F"] = "F+F--F+F";
-            _rules["+"] = "+";
-            _rules["-"] = "-";
-
             InitializeComponent();
 
             _final = _axiom;
             for(int i = 0; i < _iterations; i++) 
             {
-                _final = lsystem(_final, _rules);
+                _final = LSystem(_final, _rules);
             }
 
         }
 
-        private string lsystem(string start, Dictionary<string, string> rules)
+        private string LSystem(string start, Dictionary<char, string> rules)
         {
             var outString = "";
 
             foreach(var c in start)
             {
-                var s = rules[c.ToString()];
+                var s = rules[c];
                 outString += s;
             }
 
@@ -66,17 +66,51 @@ namespace L_System_Renderer
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
-            Pen shapeOutlinePen = new Pen(Brushes.Black, 2);
-            shapeOutlinePen.Freeze();
+            var pen = new Pen
+            {
+                Brush = Brushes.Black,
+                Thickness = 0.05f,
+                // pen.LineJoin = PenLineJoin.Round;
+                EndLineCap = PenLineCap.Round,
+                StartLineCap = PenLineCap.Round
+            };
+            pen.Freeze();
 
             // Create a DrawingGroup
             DrawingGroup dGroup = new DrawingGroup();
+
+            var currentPosition = new Point(0, 0);
+            var direction = new Point(1, 0);
 
             // Obtain a DrawingContext from 
             // the DrawingGroup.
             using (DrawingContext dc = dGroup.Open())
             {
-                
+                foreach (var c in _final)
+                {
+                    switch (c)
+                    {
+                        case 'F':
+                            var dx = direction.X * _length;
+                            var dy = direction.Y * _length;
+                            var newPos = new Point( currentPosition.X + dx, currentPosition.Y + dy);
+                            dc.DrawLine(pen, currentPosition, newPos);
+                            currentPosition = newPos;
+                            break;
+                        case '+':
+                            var angle = _angle * Math.PI / 180.0;
+                            var angleNeg = angle *= -1;
+                            direction = new Point(direction.X * Math.Cos(angleNeg) - direction.Y * Math.Sin(angleNeg),
+                                direction.X * Math.Sin(angleNeg) + direction.Y * Math.Cos(angleNeg));
+                            
+                            break;
+                        case '-':
+                            var anglePos = _angle * Math.PI / 180.0;
+                            direction = new Point(direction.X * Math.Cos(anglePos) - direction.Y * Math.Sin(anglePos),
+                                direction.X * Math.Sin(anglePos) + direction.Y * Math.Cos(anglePos));
+                            break;
+                    }
+                }
             }
 
             // Display the drawing using an image control.
